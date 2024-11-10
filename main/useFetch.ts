14639b59eb8284c04f6d1eded39f707e969d1a70
yekuuun <yekuuun@proton.me>
2024-11-10 13:10:22 +0100

    UPDATE : adding FormData post request

--- main/useFetch.ts
+++ main/useFetch.ts
@@ -213,6 +213,53 @@ export default class useFetch
         }
     }
 
+    /**
+     * base action for file upload => FORMDATA handling.
+     * @param url 
+     * @param body 
+     * @returns 
+     */
+    static callMultipartPostRequest = async<T> (url: string, body: FormData):Promise<IHttpResponse<T>> => {
+        try{
+            const response = await fetch(url, {
+                method: 'POST',
+                body: body, 
+                credentials: 'include', 
+                //multipart/form-data automatically handled.
+            });
+
+            if(!response.ok)
+            {
+                return this.buildErrorResponse(response);
+            }
+            else
+            {   
+                const data:T = await response.json() as T;
+
+                const apiResponse:IHttpResponse<T> = {
+                    "data":data,
+                    "error": null
+                }
+
+                return apiResponse;
+            }
+        }
+        catch(error)
+        {
+            const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+
+            const newExceptionResponse:IHttpResponse<T> = {
+                "data":null as unknown as T,
+                "error": {
+                    "message":errorMessage,
+                    "status":400
+                }
+            }
+
+            return newExceptionResponse;
+        }
+    }
+
 
     ///----------PRIVATE-------
 
